eb0ac9a283203013775498560c6549fb6d1d859c
aslikeyou <omenux@yandex.ru>
2014-01-06 12:10:03 +0200

    add request lib to parser.js

--- parser.js
+++ parser.js
@@ -1,42 +1,52 @@
 var read = require('node-readability');
 var fs = require('fs');
 var cheerio = require('cheerio');
+var request = require('request');
 
-module.exports  = function parser(html, callback) {
-  read(html, {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
-    }
-  },function(err, article) {
-    // if we have read error
+module.exports  = function parser(url, callback) {
+  request(url, function(err, response, html) {
     if(err) {
       callback(err);
       return ;
     }
 
-    var $ = cheerio.load(article.html);
-
-    // check for og facebook type. If exist it must be an article
-    var ogType = $("meta[property='og\\:type']").attr('content');
-
-    if(ogType !== undefined && ogType !== 'article') {
-      callback(new Error('og:type isn`t article'));
-      return;
+    if(response.statusCode !== 200) {
+      callback(new Error('Status code for your url is ' + response.statusCode + '. Must be 200!'));
+      return ;
     }
 
-    var parsedObject = {};
+    read(html, function(err, article) {
+      // if we have read error
+      if(err) {
+        callback(err);
+        return ;
+      }
 
-    var ogImage = $("meta[property='og\\:image']");
-    if(ogImage.length !== 0) {
-      parsedObject.image = ogImage.attr('content');
-    }
+      
+      var $ = cheerio.load(article.html);
 
-    //console.log(article.content);
+      // check for og facebook type. If exist it must be an article
+      var ogType = $("meta[property='og\\:type']").attr('content');
 
-    parsedObject.content = article.content;//.replace('`','\'');
-    parsedObject.title = article.title;
-    parsedObject.html = article.html;
+      if(ogType !== undefined && ogType !== 'article') {
+        callback(new Error('og:type isn`t article'));
+        return;
+      }
 
-    callback(null, parsedObject);
+      var parsedObject = {};
+
+      var ogImage = $("meta[property='og\\:image']");
+      if(ogImage.length !== 0) {
+        parsedObject.image = ogImage.attr('content');
+      }
+
+      //console.log(article.content);
+
+      parsedObject.content = article.content;//.replace('`','\'');
+      parsedObject.title = article.title;
+      parsedObject.html = article.html;
+
+      callback(null, parsedObject);
+    });
   });
 };
