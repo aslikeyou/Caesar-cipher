9f4dab35f06a2346dd3199310b32f692a58cc10a
aslikeyou <omenux@yandex.ru>
2014-01-10 17:15:44 +0200

    change node readability repo, add callbacks, fix some bugs

--- parser.js
+++ parser.js
@@ -1,25 +1,35 @@
-var read = require('node-readability');
-var fs = require('fs');
+var read = require('./node_modules/node-readability/src/readability');
+
 var cheerio = require('cheerio');
-var request = require('request');
-var urlLib = require('url');
 
 module.exports  = function parser(url, callback) {
-  request(url, function(err, response, html) {
+  var videoHandler = false;
+  read(url, {
+    cleanRulers : [
+      function(obj, tag) {
+        if(tag !== 'object') {
+          return false;
+        }
+
+        if(obj.getAttribute('class') === 'BrightcoveExperience') {
+          videoHandler = 'brightcove';
+          return true;
+        }
+
+        return false;
+      }
+    ]
+  }, function(err, article, response) {
+    // if we have read error
     if(err) {
       callback(err);
       return ;
     }
 
+    var originalUrl = response.request.uri.href
+
     try {
-      var originalUrl = response.request.uri.href;
-
-      if(response.statusCode !== 200) {
-        callback(new Error('Status code for your url is ' + response.statusCode + '. Must be 200!'));
-        return ;
-      }
-
-      var $ = cheerio.load(html);
+      var $ = cheerio.load(article.html);
       // check for og facebook type. If exist it must be an article
       var ogType = $("meta[property='og\\:type']").attr('content');
 
@@ -35,65 +45,24 @@ module.exports  = function parser(url, callback) {
       if(ogImage.length !== 0) {
         parsedObject.image = ogImage.attr('content');
       }
-
-      html = $.html();
     } catch (e) {
       callback(e);
       return ;
     }
 
-    read(html, function(err, article) {
-      // if we have read error
-      if(err) {
-        callback(err);
-        return ;
-      }
-      try {
-        var $ = cheerio.load(article.content);
+    parsedObject.content = article.content;
+    parsedObject.title = article.title;
+    parsedObject.html = article.html;
+    parsedObject.originalUrl = originalUrl;
+    if(videoHandler) {
+      parsedObject.videoHandler = {
+        name : videoHandler,
+        toHead : '<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>',
+        toBody : '<script type="text/javascript">brightcove.createExperiences();</script>'
 
-        $('img, a').each(function() {
-          var that = $(this);
-          var src = null;
-          var tagName = that[0].name.toLowerCase();
+      };
+    }
 
-
-          switch (tagName) {
-            case 'a' :
-              src = that.attr('href');
-              break;
-            case 'img' :
-              src = that.attr('src');
-              break;
-          }
-
-          if(!src) {
-            that.remove();
-            return ;
-          }
-
-          var resultUrl = function(src) {
-            return urlLib.resolve(originalUrl, src);
-          }(src);
-
-          switch (tagName) {
-            case 'a' :
-              that.attr('href', resultUrl);
-              break;
-            case 'img' :
-              that.attr('src',  resultUrl);
-              break;
-          }
-        });
-      } catch (e) {
-        callback(e);
-        return;
-      }
-      parsedObject.content = $.html();//.replace('`','\'');
-      parsedObject.title = article.title;
-      parsedObject.html = article.html;
-      parsedObject.originalUrl = originalUrl;
-
-      callback(null, parsedObject);
-    });
+    callback(null, parsedObject);
   });
 };
